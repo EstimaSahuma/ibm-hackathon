d95e493c3b32cd24bb94aa41777f5d64a2f91d53
estima.sahuma <estima.sahuma@ideiasdinamicas.com>
2025-06-28 05:36:38 +0100

    fix: add language model

--- backend/utils/extract_text.py
+++ backend/utils/extract_text.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import pytesseract
 
-def extract_text_image(caminho):
-    texto = pytesseract.image_to_string(Image.open(caminho), lang='por')
+def extract_text_image(caminho, idioma: str = 'pt'):
+    texto = pytesseract.image_to_string(Image.open(caminho), lang=idioma)
     return texto
